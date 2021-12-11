7e4fbed73071681fa38271524e55e72fcb75e7d3
Nikola Ninkov <nikola.n.ninkov@gmail.com>
2021-12-11 22:35:29 +0200

    Test commit

--- client/src/components/dashboard/StudentDashboard.tsx
+++ client/src/components/dashboard/StudentDashboard.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 export default function StudentDashboard() {
-  return <div className='dashboard-student'></div>;
+  return <div className='dashboard-student'>firebase</div>;
 }
